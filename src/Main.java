f7dfec4f58938e1d52e5002496c603706fdb8499
SOLO-IMPERATOR <levchenko563@yandex.ru>
2022-08-23 15:44:05 +0300

    Three commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String [] args){
         System.out.println("Hello world!");
         System.out.println("Show me");
+        System.out.println("Third project");
     }
 }
